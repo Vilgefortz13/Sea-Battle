c86701000b3de9438f2c33f9c44e2d7c3da832aa
Rinat Uzanzaev <wilax175@gmail.com>
2022-06-23 16:23:52 +0600

    initialize and print field

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,22 @@ public class Main {
 
     private static void run() {
         String[][] field = new String [9][9];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = " ";
+            }
+        }
+
+        int verticalCoordinates = 1;
+
+        System.out.println("\tA   B   C   D   E   F   G   H   I");
+        for (int i = 0; i < field.length; i++) {
+            System.out.print(verticalCoordinates + " | ");
+            verticalCoordinates++;
+            for (int j = 0; j < field.length; j++) {
+                System.out.print(field[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 }
